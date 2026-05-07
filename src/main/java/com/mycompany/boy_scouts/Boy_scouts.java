f9e9dd21e25e98dd9366d019f4cffd1e58366bed
zyin-08 <zhyin2608@gmail.com>
2026-05-07 12:06:17 +0200

    Start project

---
+++ src/main/java/com/mycompany/boy_scouts/Boy_scouts.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.boy_scouts;
+
+/**
+ *
+ * @author zyin2
+ */
+public class Boy_scouts {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Boy Scouts!!");
+    }
+}
