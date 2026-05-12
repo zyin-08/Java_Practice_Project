59a68ad78141ed94dbf102bf0c77e2d00dd56eda
zyin-08 <zhyin2608@gmail.com>
2026-05-12 11:51:07 +0200

    Refactor update values in a menu

--- src/main/java/com/mycompany/boy_scouts/Boy_scouts.java
+++ src/main/java/com/mycompany/boy_scouts/Boy_scouts.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.boy_scouts;
 
 import java.util.Scanner;
@@ -15,37 +14,83 @@ public class Boy_scouts {
     private static int numberCampers = 10;
     private static String location = "Barcelona";
     private static int days = 5;
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("====== Welcome to Scout Camp Manager ======");
-        
+        int opc;
+        do {
+            opc = menu();
+            switch (opc) {
+                case 1:
+                    showCamp();
+                    break;
+                case 2:
+                    updateNumber();
+                    break;
+                case 3:
+                    updateLocation();
+                    break;
+                case 4:
+                    updateDays();
+                    break;
+            }
+        } while (opc != 0);
+
         System.out.println("Enter your scout leader name:");
         String name = sc.nextLine();
         System.out.println("Enter your camp name:");
         String campName = sc.nextLine();
         System.out.println("Leader name: " + name);
         System.out.println("Camp name: " + campName);
-        
+
+    }
+
+    public static int menu() {
+        System.out.println("====== Welcome to Scout Camp Manager ======");
+        System.out.println("1. Show camp info");
+        System.out.println("2. Update number of campers");
+        System.out.println("3. Update location");
+        System.out.println("4. Update days");
+        System.out.println("0. Exit");
+
+        Scanner sc = new Scanner(System.in);
+        int opc;
+        do {
+            System.out.println("Elige una opcion: ");
+            opc = sc.nextInt();
+        } while (opc < 0 || opc > 5);
+        return opc;
+    }
+
+    public static void showCamp() {
         System.out.println("====== Camp info ======");
         System.out.println("Number of campers: " + numberCampers);
         System.out.println("Location: " + location);
         System.out.println("Days: " + days);
-        
+    }
+
+    public static void updateNumber() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Update number of campers: ");
         numberCampers = sc.nextInt();
         System.out.println("Thanks! Number of campers changed to: " + numberCampers);
-        
+
+        if (numberCampers > 50) {
+            System.out.println("Large Camp");
+        }
+    }
+
+    public static void updateLocation() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Update location: ");
         location = sc.nextLine();
         System.out.println("Thanks! Location changed to: " + location);
-        
+    }
+
+    public static void updateDays() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Update days: ");
         days = sc.nextInt();
         System.out.println("Thanks! Days changed to: " + days);
-        
-        if (numberCampers > 50){
-            System.out.println("Large Camp");
-        }
     }
 }
